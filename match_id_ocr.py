0055cf04326a24aa5961e02357f55b64bbd00cfb
jakep <jakeparperis@gmail.com>
2025-10-04 17:54:58 +0100

    Implement match_results_id_ocr() to allow the bot to save and process iamges sent in match-results channel

--- match_id_ocr.py
+++ match_id_ocr.py
@@ -1,9 +1,11 @@
 import easyocr
 import re
+import tempfile
+from run import bot, ALLOWED_CHANNELS
 
 reader = easyocr.Reader(['en'])
 
-# --- Returns match ID ---
+
 def get_match_id(img_path):
     results = reader.readtext(img_path) # stores text by section
     for _, text, prob in results:
@@ -13,3 +15,22 @@ def get_match_id(img_path):
             return match_id
 
     return None
+
+# --- temporarily save images sent in match-results and extract the match id ---
+async def match_results_id_ocr(message):
+    if message.author == bot.user:
+        return
+
+    if message.channel.name != "match-results" or message.channel.name not in ALLOWED_CHANNELS:
+        return
+
+    if message.attachments:
+        for attachment in message.attachments:
+            if attachment.filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+                with tempfile.NamedTemporaryFile(delete=True, suffix=attachment.filename) as temp_img:
+                    await attachment.save(temp_img.name)
+                    match_id = get_match_id(temp_img.name)
+                    if match_id:
+                        await message.channel.send(f"Match ID: {match_id}")
+                    else:
+                        await message.channel.send("Match ID not found.")
